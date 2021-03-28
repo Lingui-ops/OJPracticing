9800f502b95bf11d524665d1711a091699dc5d60
LZY <te1900@foxmail.com>
2021-03-28 15:20:55 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+namespace _nmsp {
+	class A {
+	public:
+		int m_i = 1;
+		A() { cout << "A()" << endl; }
+		~A() { cout << "~A()" << endl; }
+	};
+	class B {
+	public:
+		inline static A m_sa;
+	};
+}
+class TE {
+public:
+	TE() { cout << "111111111" << endl; }
+	~TE() { cout << "222222222" << endl; }
+};
+class Foo {
+public:
+	inline static TE t;
+};
+int main() {
+	//_nmsp::B b;
+	//cout << b.m_sa.m_i << endl;
+
+	return 0;
+}
